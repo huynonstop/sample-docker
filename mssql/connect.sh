e5ecabc94dd0870ee98c4548b9d1d7642fcc9dc6
huynonstop <tthuykh99@gmail.com>
2021-04-21 00:33:37 +0700

    update"

---
+++ mssql/connect.sh
@@ -0,0 +1,5 @@
+docker ps
+
+docker exec -it <id || name> bash
+
+/opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U SA -P Secret1234
---
+++ postgres/connect.sh
@@ -0,0 +1,5 @@
+docker ps
+
+docker exec -it <id || name> bash
+
+psql -h localhost -p 5432 -d postgres -U postgres -P postgres
